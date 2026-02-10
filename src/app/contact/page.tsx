9f6ce06659c38923a4ddeb7c34992165bdae530d
Manny-Ventura <wow.mannyv@gmail.com>
2026-02-10 14:07:38 -0500

    removed svgs, cleaned up global styles, added to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,17 @@
+import { Mail, Linkedin, Github } from 'lucide-react'
+
+type ContactItemProps = {}
+
 export default function Page() {
   return (
     <>
       <h1>Contact Me!</h1>
-      <a href="mailto:mannyventura9@yahoo.com, mventuraprogramming@gmail.com">Email</a>
+      <section className="grid grid-rows-2 gap-4">
+        <div className="flex">
+          <span className="mr-2 font-bold">Email:</span>
+          <a href="mailto:mannyventura9@yahoo.com, mventuraprogramming@gmail.com"></a>
+        </div>
+      </section>
     </>
   )
 }
