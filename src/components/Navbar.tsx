5f1eea8eb95e6fa5d8f3f1fefcf81d0a2452cbb2
Manny-Ventura <wow.mannyv@gmail.com>
2026-02-25 16:26:47 -0500

    added name to header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,9 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <nav className="flex justify-around p-4">
+      <div>
+        <h3>Manny Ventura</h3>
+      </div>
       <Link
         href="/"
         className="duration:150 border-b-2 border-transparent text-[var(--text-secondary)] transition-colors hover:border-[var(--accent)] hover:text-[var(--text-primary)]"
