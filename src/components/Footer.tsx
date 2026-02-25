40364a2c5df3ac38ea55c278f7e07ea52f3d1839
Manny-Ventura <wow.mannyv@gmail.com>
2026-02-25 16:26:28 -0500

    created footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,25 @@
+import { Github, Linkedin } from 'lucide-react'
+
 export default function Footer() {
-  return <div>Footer!</div>
+  return (
+    <div className="flex justify-around">
+      <div className="text-[var(--text-secondary)]">Manny Ventura © 2026</div>
+      <div className="flex">
+        <a
+          href="https://github.com/Manny-Ventura"
+          target="_blank"
+          className="px-2 text-[var(--text-secondary)] transition-colors hover:text-[var(--text-primary)]"
+        >
+          <Github />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/manny-ventura-048573169/"
+          target="_blank"
+          className="px-2 text-[var(--text-secondary)] transition-colors hover:text-[var(--text-primary)]"
+        >
+          <Linkedin />
+        </a>
+      </div>
+    </div>
+  )
 }
