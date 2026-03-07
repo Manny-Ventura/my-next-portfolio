d07afedf39c944df03c997cf8d27a4044e456365
Manny-Ventura <wow.mannyv@gmail.com>
2026-03-07 17:27:31 -0500

    slightly better texst for about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,16 @@
 export default function Page() {
   return (
-    <>
+    <div className="flex flex-col gap-4 text-center">
       <h1>About Me...</h1>
       <h2>Hello, my name is Manuel Ventura!</h2>
-      <p>I am a Software Developer out of upstate South Carolina.
+      <p className="md:w-md lg:w-lg">I am a Software Developer out of upstate South Carolina.
         I began my programming journey during COVID, and have continued building my skills since.
         I earned my Microcomputer Programming Certificate from my local college, then immediately went professional.
         I have since been hired as a Full-Stack Web Developer, leading front-end development for projects with a MSP.
         I have also worked with backend-design including API Development & Management, as well as automated deployment and configuration.
         I have professional experience with Python, React, HTMX, NoSQL, Javascript, and more. 
-        I am always looking to expand my skillset and learn new technologies, and I am excited to see where my career takes me!
+        I am always looking to expand my skillset and learn new technologies.
       </p>
-    </>
+    </div>
   )
 }
